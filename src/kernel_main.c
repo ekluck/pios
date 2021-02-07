891e51870c2cdc04671a202ff7be9181ac184cf9
ekluck <ekluck@luc.edu>
2021-02-07 11:04:22 -0600

    files erroring

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,36 @@
+#include <stdio.h>
+#include "list.h"
 
+void bss_to_zero();
+
+extern int __bss_start;
+extern int __bss_end;
+
+int global;
+
+struct list_element b = {NULL,NULL,1};
+struct list_element a = {NULL,NULL,5};
+struct list_element c = {NULL,NULL,2};
+struct list _element *head= &a;
+
+struct list_element* list = &a
 
 
 void kernel_main() {
-
-    while(1){
+	bss_to_zero();
+	list_add(list, &b);
+	list_add(list, &c);
+	list_remove(head,1);
+   	while(1){
     }
+}
+
+void bss_to_zero(){
+	(&__bss_start[0] = 0x0c;
+	int x =0;
+	while ((&__bss_start)+x != &__bss_end){
+		(&__bss_start)[x] = 0;
+		x++;
+	}
+
 }
