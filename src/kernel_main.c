45202f8c7e10de373ced8edbda8774039b2812b7
ekluck <ekluck@luc.edu>
2021-03-16 22:12:39 -0500

    hw

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,37 +1,88 @@
-//#include <stdio.h>//comment this out for serial port hw
-#include "led.h"
+#include "list.h"
 #include "serial.h"
-#include "rprint.h"
+#include "rprintf.h"
+#include "page.h"
 
 int global;
-#define NULL (void*)0; //added for serial port hw in tuesday class
+#define NULL (void*)0
 extern long __bss_start;
 extern long __bss_end;
+extern struct ppage* free_list;
+
+unsigned int *gpset1 = 0xFE200020;
+unsigned int *gpclr1 = 0xFE20002C;
+unsigned int *gpsel4 = 0xFE200010;
+
+//HW1 Office Hours 2/1 w/ Jack
+void clear_bss()
+
+{
+	int x = 0;
+	while ((&__bss_start)+x != &__bss_end){
+		(&__bss_start)[x] = 0;
+		x++;
+		}
+}
 
 
-//void clear_bss(){
-	
-//	char * bssStart = &__bss_start;
-//	char * bssEnd = &__bss_end;
-//	for(int i = 0; i+(&__bss_start) < __bss_end; i++){
-//		(&__bss_start)[i] = 0;
-//	}
-//}
 
-int *mu_io_reg = 0x3F215040; //added for serial port hw in tuesday class
+//hw4
+void led_init(){
+	unsigned int mask_number = 0xFFFFFE3F;
+	*gpsel4 = *gpsel4 & mask_number;
+	*gpsel4 = *gpsel4 | (1 << 6);
+	return;
+}
+
+void led_on(){
+	*gpset1 = (1 << 10);
+}
+
+void led_off(){
+	*gpclr1 = (1 << 10);
+}
+
+void delay (unsigned int d){
+	unsigned int i, x;
+
+	for(i = 0; i < 0x7f; i++) {
+		for (x = 0; x < d; x++){
+			asm("nop");
+		}
+	}
+
+}
+
+int getEL(){
+	unsigned int el;
+
+	asm("mrs %0,CurrentEL"
+		: "=r"(el)
+		:
+		:);
+
+}
+
+
 void kernel_main(){
-//	clear_bss();
-//	led_init();
-	int *mu_io_reg = 0x7E215040;
-	*mu_io_reg = 'E';
-	*mu_io_reg = 'V';
-	*mu_io_reg = 'I';
-	*mu_io_reg = 'E';
-	esp_printf(putc, "The memory location is %x", kernel_main); //added for serial port hw tuesday class
-    while(1){
-//	led_on(); //comment these out before commit and pushing serial port hw
-//	delay(10);
-//	led_off();
-//	delay(10);
-    }
+	clear_bss();
+
+        init_pfa_list();
+
+	struct ppage* test = free_list->next;
+	esp_printf(putc, "Physical addr: %x \n", test->physical_addr);
+	test = allocate_physical_pages(2);
+	esp_printf(putc, "Physical addr (test) after npages: %x \n", test);
+	esp_printf(putc, "Physical addr (->physical_addr) after npages: %x \n", test->physical_addr);
+	free_physical_pages(test);
+	test = free_list->next;
+	esp_printf(putc, "Post free addr: %x \n", test->physical_addr);
+
+
+
+ 	//mmu_on();
+
+	while(1){
+
+	}
 }
---
+++ src/page.c
@@ -0,0 +1,40 @@
+extern int __end;
+#include "list.h"
+#include "page.h"
+#define NPAGES 128
+#define NULL (void*)0
+
+struct ppage physical_page_array[NPAGES];
+struct ppage* free_list = NULL;
+
+
+void init_pfa_list(void){
+	void* address_p = (((unsigned long)&__end) & ~(0x200000 - 1)) + 0x200000;
+	int x;
+	for (x=0; x<NPAGES; x++){
+		listAdd(&free_list, (struct listElement*) &physical_page_array[x]);
+		physical_page_array[x].physical_addr = address_p;
+		address_p += 0x200000;
+	}
+}
+
+struct ppage *allocate_physical_pages(unsigned int npages){
+	struct ppgage* new_var = free_list;
+	struct ppage* new_list = NULL;
+	int x;
+	for (x=0; x<NPAGES; x++){
+		listRemove(new_var);
+		listAdd(&new_list, new_var);
+	}
+	return new_list;
+}
+
+void free_physical_pages(struct ppage *ppage_list){
+//copy allocate
+//remove from ppage list and add  to free list
+	struct ppage* new_var = ppage_list;
+	while (ppage_list != NULL){
+		listRemove(new_var);
+		listAdd(&free_list, new_var);
+	}
+}
